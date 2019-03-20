85d3602e8f0f38eff903d217378c33693f02a78a
Ralph Lange (CR/AEE1) <ralph.lange@de.bosch.com>
2019-03-20 08:34:10 +0100

    Ported original keyboard teleop code from ROS 1 under BSD.
    Signed-off-by: Ralph Lange (CR/AEE1) <ralph.lange@de.bosch.com>

--- simple_keyboard_teleop/simple_keyboard_teleop/simple_keyboard_teleop.py
+++ simple_keyboard_teleop/simple_keyboard_teleop/simple_keyboard_teleop.py
@@ -1,6 +1,6 @@
 import math
 
-import sys, termios, tty, os, time, threading
+import sys, select, termios, tty, os, time
 
 import rclpy
 from rclpy.clock import Clock
@@ -8,86 +8,139 @@ from rclpy.node import Node
 from geometry_msgs.msg import Twist
 
 
+msg = """
+Reading from the keyboard  and Publishing to Twist!
+---------------------------
+Moving around:
+   u    i    o
+   j    k    l
+   m    ,    .
+For Holonomic mode (strafing), hold down the shift key:
+---------------------------
+   U    I    O
+   J    K    L
+   M    <    >
+t : up (+z)
+b : down (-z)
+anything else : stop
+q/z : increase/decrease max speeds by 10%
+w/x : increase/decrease only linear speed by 10%
+e/c : increase/decrease only angular speed by 10%
+CTRL-C to quit
+"""
+
+moveBindings = {
+        'i':(1,0,0,0),
+        'o':(1,0,0,-1),
+        'j':(0,0,0,1),
+        'l':(0,0,0,-1),
+        'u':(1,0,0,1),
+        ',':(-1,0,0,0),
+        '.':(-1,0,0,1),
+        'm':(-1,0,0,-1),
+        'O':(1,-1,0,0),
+        'I':(1,0,0,0),
+        'J':(0,1,0,0),
+        'L':(0,-1,0,0),
+        'U':(1,1,0,0),
+        '<':(-1,0,0,0),
+        '>':(-1,-1,0,0),
+        'M':(-1,1,0,0),
+        't':(0,0,1,0),
+        'b':(0,0,-1,0),
+    }
+
+speedBindings={
+        'q':(1.1,1.1),
+        'z':(.9,.9),
+        'w':(1.1,1),
+        'x':(.9,1),
+        'e':(1,1.1),
+        'c':(1,.9),
+}
+
+
 class SimpleKeyboardTeleop(Node):
 
 
-    def read_char_from_stdin(self):
-        fd = sys.stdin.fileno()
-        original_settings = termios.tcgetattr(fd)
-        try:
-            tty.setraw(sys.stdin.fileno())
-            while self.last_input_char != 'e':
-                self.last_input_char = sys.stdin.read(1)
-                time.sleep(0.01)
-        finally:
-            termios.tcsetattr(fd, termios.TCSADRAIN, original_settings)
-            self.destroy_node()
-            rclpy.shutdown()
+    def getKey(self):
+        tty.setraw(sys.stdin.fileno())
+        sr, sw, se = select.select([sys.stdin], [], [], 0.5)
+        key = 'k'
+        if sr != [] or sw != [] or se != []:
+            key = sys.stdin.read(1)
+            termios.tcsetattr(sys.stdin, termios.TCSADRAIN, self.settings)
+        return key
 
-    
-    def print_help(self):
-        print("  Linear speed    Angular speed    Control    E=Exit")
-        print("       Q                W           U I O           ")
-        print("       A                S           J   L           ")
-        print("                                    M , .           ")
+
+    def vels(self,speed,turn):
+        return "currently:\tspeed %s\tturn %s " % (speed,turn)
 
 
     def __init__(self):
-        super().__init__('simple_keyboard_teleop')
+        self.settings = termios.tcgetattr(sys.stdin)
 
-        self.print_help()
+        super().__init__('simple_keyboard_teleop')
 
         self.pub_cmd_vel = self.create_publisher(Twist, "/cmd_vel")
 
-        self.linear_speed_percent = 50
-        self.angular_speed_percent = 50
-
-        self.last_input_char = ' '
-
-        self.MAX_LINEAR_SPEED = 0.5
-        self.MAX_ANGULAR_SPEED = math.pi / 10.0
-
-        self.circle_timer = self.create_timer(0.1, self.timer_callback)
-
-        keyboard_thread = threading.Thread(target=SimpleKeyboardTeleop.read_char_from_stdin, args=(self,))
-        keyboard_thread.start()
+        print(msg)
 
 
-    def timer_callback(self):
-        if self.last_input_char == 'q':
-            self.linear_speed_percent = min([self.linear_speed_percent + 10, 100])
-        elif self.last_input_char == 'a':
-            self.linear_speed_percent = max([self.linear_speed_percent - 10, 0])
+    def run(self):
+        speed = 0.5
+        turn = 1.0
+        x = 0.0
+        y = 0.0
+        z = 0.0
+        th = 0.0
+        status = 0
 
-        linear_speed = 0.0
-        if self.last_input_char in ['u', 'i', 'o']:
-            linear_speed = self.linear_speed_percent * self.MAX_LINEAR_SPEED / 100.0
-        elif self.last_input_char in ['m', ',', '.']:
-            linear_speed = - self.linear_speed_percent * self.MAX_LINEAR_SPEED / 100.0
+        try:
+            while(1):
+                key = self.getKey()
+                if key in moveBindings.keys():
+                    x = moveBindings[key][0]
+                    y = moveBindings[key][1]
+                    z = moveBindings[key][2]
+                    th = moveBindings[key][3]
+                elif key in speedBindings.keys():
+                    speed = speed * speedBindings[key][0]
+                    turn = turn * speedBindings[key][1]
 
-        if self.last_input_char == 'w':
-            self.angular_speed_percent = min([self.angular_speed_percent + 10, 100])
-        elif self.last_input_char == 's':
-            self.angular_speed_percent = max([self.angular_speed_percent - 10, 0])
+                    print(self.vels(speed,turn))
+                    if (status == 14):
+                        print(msg)
+                    status = (status + 1) % 15
+                else:
+                    x = 0.0
+                    y = 0.0
+                    z = 0.0
+                    th = 0.0
+                    if (key == '\x03'):
+                        break
 
-        angular_speed = 0.0
-        if self.last_input_char in ['u', 'j', 'm']:
-            angular_speed = self.angular_speed_percent * self.MAX_ANGULAR_SPEED / 100.0
-        elif self.last_input_char in ['o', 'l', '.']:
-            angular_speed = - self.angular_speed_percent * self.MAX_ANGULAR_SPEED / 100.0
+                twist = Twist()
+                twist.linear.x = x*speed; twist.linear.y = y*speed; twist.linear.z = z*speed;
+                twist.angular.x = 0.0; twist.angular.y = 0.0; twist.angular.z = th*turn
+                self.pub_cmd_vel.publish(twist)
 
-        msg = Twist()
-        msg.linear.x = linear_speed
-        msg.angular.z = angular_speed
-        self.pub_cmd_vel.publish(msg)
+        except Exception as e:
+            print(e)
 
-        self.last_input_char = ' '
+        finally:
+            twist = Twist()
+            twist.linear.x = 0.0; twist.linear.y = 0.0; twist.linear.z = 0.0
+            twist.angular.x = 0.0; twist.angular.y = 0.0; twist.angular.z = 0.0
+            self.pub_cmd_vel.publish(twist)
+
+            termios.tcsetattr(sys.stdin, termios.TCSADRAIN, self.settings)
 
 
 def main(args=None):
     rclpy.init(args=args)
     node = SimpleKeyboardTeleop()
-    rclpy.spin(node)
+    node.run()
 
 
 if __name__ == '__main__':
