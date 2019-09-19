dc391bfeae520dcb7ec76b3d93c9d883b65ee089
Knese Karsten (CR/RTC-HMI4) <karsten.knese@us.bosch.com>
2019-09-18 23:54:41 -0700

    install python marker file
    Signed-off-by: Knese Karsten (CR/RTC-HMI4) <karsten.knese@us.bosch.com>

--- micro-ros_kobuki_demo_remote/setup.py
+++ micro-ros_kobuki_demo_remote/setup.py
@@ -23,6 +23,8 @@ setup(
     version='0.0.1',
     packages=find_packages(exclude=['test']),
     data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
         ('share/' + package_name + '/config/', ['config/joy.params',
                                                 'config/kobuki.rviz']),
