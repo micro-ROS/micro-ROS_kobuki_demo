90c554c5f3bb03999260716daeae76bfaa8b78ed
Ralph Lange (CR/AEE1) <ralph.lange@de.bosch.com>
2019-09-19 09:24:26 +0200

    Install Python marker file.
    Signed-off-by: Ralph Lange (CR/AEE1) <ralph.lange@de.bosch.com>

--- simple_keyboard_teleop/setup.py
+++ simple_keyboard_teleop/setup.py
@@ -8,6 +8,8 @@ setup(
     version='0.0.1',
     packages=find_packages(exclude=['test']),
     data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
     ],
     install_requires=['setuptools'],
