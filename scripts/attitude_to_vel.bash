829473975099503f4a5dc9351564bfb638f45a21
Pablo Garrido <pablogs9@gmail.com>
2019-10-22 12:14:01 +0200

    Launch scripts update

---
+++ scripts/attitude_to_vel.bash
@@ -0,0 +1,4 @@
+cd crazyflie_demo
+source /opt/ros/dashing/setup.bash
+source install/local_setup.bash
+ros2 launch micro-ros_crazyflie_demo_remote attitude_to_vel
--- scripts/crazyflierviz_position.bash
+++ scripts/crazyflierviz_position.bash
@@ -1,4 +1,4 @@
 cd crazyflie_demo
 source /opt/ros/dashing/setup.bash
 source install/local_setup.bash
-ros2 launch micro-ros_crazyflie_demo_remote remote_without_control.launch.py
+ros2 launch micro-ros_crazyflie_demo_remote launch_drone_position.launch.py
