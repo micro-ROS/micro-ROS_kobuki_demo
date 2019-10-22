6db6d6ea6ac91ac101503ac0742a12071eb54db3
Pablo Garrido <pablogs9@gmail.com>
2019-10-22 07:33:15 +0200

    Crazyflie init script

--- scripts/crazyflie_clientbridge.bash
+++ scripts/crazyflie_clientbridge.bash
@@ -1,8 +1,9 @@
 source /opt/ros/dashing/setup.bash
+rm /used_serialport.txt
 python3 /crazyflie-clients-python/bin/cfclient &> /dev/null &
 while [ ! -f /used_serialport.txt ] ;
 do
       sleep 0.1
 done
 
-MicroXRCEAgent serial --dev $(cat used_serialport.txt) &> /dev/null < /dev/null & 
+MicroXRCEAgent serial --dev $(cat used_serialport.txt)  &> /dev/null < /dev/null & 
